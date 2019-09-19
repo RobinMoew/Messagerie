e7ccca6a047aa2456d14ed0da86566f37166f718
RobinMoew <robin.marcot1992@gmail.com>
2019-09-19 13:44:56 +0200

    terminé

--- js/messagerie.js
+++ js/messagerie.js
@@ -1,7 +1,27 @@
-let pseudos = ['Momo', 'Robin', 'Jesus', 'Ricardo'];
+let pseudos = ['Momo', 'Robin', 'Jesus'];
 let display_message = $('#messages');
 let input = $('#message');
+let id = 0;
+let class_nb = 0;
+let smileys_shown = false;
+let smileys = ['😃', '👽', '🖕'];
 
+$('#smileys').hide();
+for (let i = 0; i < smileys.length; i++) {
+  $('#smileys').append(`
+    <div class='smiley' id='smiley${i}'>${smileys[i]}</div>
+  `);
+}
+
+$('#btn_smileys').click(() => {
+  if (!smileys_shown) {
+    smileys_shown = true;
+    $('#smileys').show();
+  } else {
+    $('#smileys').hide();
+    smileys_shown = false;
+  }
+});
 
 $('#send').click(() => {
   let pseudo = pseudos[Math.floor(Math.random() * pseudos.length)];
@@ -11,10 +31,10 @@ $('#send').click(() => {
     alert("C'est un chat, c'est fait pour discuter. Tu vas pas dans un bar pour boire de l'eau !");
   } else {
     display_message.append(`
-      <div class='message_container'>
-        <div class='pseudo'><img class="imagePseudo" src="https://upload.wikimedia.org/wikipedia/commons/f/f4/User_Avatar_2.png">${pseudo}:</div>
-        <div class='message'>${message}</div>
-      </div>
+    <div class='message_container' id="${id}">
+      <div class='pseudo ${class_nb}'><img class="imagePseudo" src="https://upload.wikimedia.org/wikipedia/commons/f/f4/User_Avatar_2.png">${pseudo}:</div>
+      <div class='message'>${message}</div>
+    </div>
     `);
     display_message.animate(
       {
@@ -22,7 +42,27 @@ $('#send').click(() => {
       },
       500
     );
+
     input.val('');
+
+    /*     for (let i = 0; i < display_message[0].children.length; i++) {
+      pseudo = display_message[0].children[i].firstChild.nextSibling.innerText;
+      color = display_message[0].children[i].firstChild.nextSibling.style.backgroundColor;
+
+      if (
+        $('.' + class_nb)[0].className !=
+        display_message[0].children[i].firstChild.nextSibling.className
+      ) {
+        $('.' + class_nb).css('background-color', getRandomColor());
+        class_nb++;
+      } else {
+        $('.' + class_nb).css('background-color', $('.' + class_nb).css('background-color'));
+      }
+    } */
+
+    $('#' + id).css('background-color', getRandomColor());
+
+    id++;
   }
 
   $.ajax({
@@ -44,16 +84,27 @@ $('#reset').click(() => {
     type: 'GET',
     success: (result) => {
       let json = JSON.parse(result);
-      console.log(json);
 
       for (let i = 0; i < json.length; i++) {
         display_message.append(`
-          <div class='message_container'>
+          <div class='message_container' id="${id}">
             <div class='pseudo'>${json[i].pseudo}:</div>
             <div class='message'>${json[i].message}</div>
-          </div>
-        `);
+          </div>`);
+
+        $('#' + id).css('background-color', getRandomColor());
+
+        id++;
       }
     }
   });
 });
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
